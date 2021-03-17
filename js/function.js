b85b14b26ee94883f6520cbdff08f36a03928166
phyrrus <andrea.pirrone@protonmail.com>
2021-03-17 20:28:48 +0000

    cutting

--- js/function.js
+++ js/function.js
@@ -48,20 +48,6 @@ $(document).on('change', '#first-selection', function(){
 });
 
 $(document).on('change', '#second-selection',function(){
-    $.getJSON('js/etf_ticker_fundname.json',function(data){
-        var key = $('#second-selection').val();
-        var vals = '';
-        switch(key){
-            case 'VAGP':
-                vals = data.VAGP;
-                JsonName = vals;
-                break;
-
-        }
-        ItemName = JsonName;
-        $('#quantity-text'+GBCount).text(JsonName);
-        $('#item-quantity'+GBCount).val(JsonName);
-    });
     $('#first-text'+GBCount).text($('#first-selection').val());
     $('#second-text'+GBCount).text($('#second-selection').val());
 });
@@ -85,7 +71,6 @@ function CheckPercentage(){
 }
 
 function ChangeDynamic(prmPercentage){
-    var ItemPicetext = 0;
     var ID  = parseInt($('#percentage-text'+GBCount).attr('sub-id'));
     GBQuantiy = parseInt(prmPercentage.value);
     if(ID === GBCount){
@@ -106,3 +91,4 @@ $('#Btndelete').click(function(){
         console.log('error');
     }
 });
+
