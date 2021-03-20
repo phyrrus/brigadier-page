c8cf33aa5d683ccb22ff52ba0af9223e8826f490
phyrrus <andrea.pirrone@protonmail.com>
2021-03-20 16:07:53 +0000

    testing moving function out

--- js/function.js
+++ js/function.js
@@ -1,29 +1,35 @@
 var GBQuantiy = 0, GBCount = 1, Provider = '', Ticker = ''; 
 $(document).ready(function(){
     firstSelectionDD();    
-    $("#user-data-form").submit(function(){
-        const firstName = $("#first-name-text").val();
-        const lastName = $("#last-name-text").val();
-        const email = $("#email-text").val();
-        //var portfolio = document.getElementById('item-table').childNodes;
-        const payload = {
-            firstName,
-            lastName,
-            email//,
-            //portfolio
-        };
-        // Please update this url after deploying this functiont to azure with azure function url 
-        fetch("https://brigadier-backend-functions.azurewebsites.net/api/form_to_userdb?", {
-          method: "post",
-          body: JSON.stringify(payload),
-        })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            alert("Request submitted ...");
-            console.log(data);
-        });
+});
+$("#user-data-form").on('submit',function(){
+    const firstName = $("#first-name-text").val();
+    const lastName = $("#last-name-text").val();
+    const email = $("#email-text").val();
+    var portfolio = {};
+    var table = document.getElementById("item-table");
+    var r=0; //start counting rows in table
+    while(row=table.rows[r++])
+    {
+        portfolio[$('#second-text'+GBCount).val()] = $('#percentage-text'+GBCount).val()
+    }
+    const payload = {
+        firstName,
+        lastName,
+        email,
+        portfolio
+    };
+    // update this url after deploying function to azure 
+    fetch("https://brigadier-backend-functions.azurewebsites.net/api/form_to_userdb?", {
+      method: "post",
+      body: JSON.stringify(payload),
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        alert("Request submitted ...");
+        console.log(data);
     });
 });
 $('#Btnadd').click(function(){
