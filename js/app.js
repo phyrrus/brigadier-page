167b54f580c841dd35bf0ef73d7af70d1f6f00b7
phyrrus <andrea.pirrone@protonmail.com>
2021-05-09 13:49:26 +0100

    Testing form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,59 @@
 
+$(document).ready(function () {
+    var counter = 0;
+
+    $("#addrow").on("click", function () {
+        var newRow = $("<tr>");
+        var cols = "";
+
+        cols += '<td><select class="form-control" id="first-selection' + counter + '"/><option selected value=""></option></td>';
+        cols += '<td><select class="form-control" id="second-selection' + counter + '"/><option selected value=""></option></td>';
+        cols += '<td><input type="number" class="form-control" placeholder = "%" id="inputPercentage' + counter + '"/></td>';
+
+        cols += '<td><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Delete"></td>';
+        newRow.append(cols);
+        $("table.order-list").append(newRow);
+        counter++;
+    });
+
+    $("table.order-list").on("click", ".ibtnDel", function (event) {
+        $(this).closest("tr").remove();       
+        counter -= 1
+    });
+});
+
+
+function firstSelectionDD() {
+    $.get('js/etf_provider_ticker.json', function (data) {
+        $.each(data, function (index) {
+            $('#first-selection').append('<option value="' + index + '">' + index + '</option>');
+        });
+    });
+}
+
+$(document).on('change', '#first-selection', function () {
+    var dropdown = $(this);
+    $.getJSON('js/etf_provider_ticker.json', function (data) {
+        var key = dropdown.val();
+        var vals = [];
+        switch (key) {
+            case 'Vanguard':
+                vals = data.Vanguard.split(',');
+                break;
+            case 'Blackrock':
+                vals = data.Blackrock.split(',');
+                break;
+        }
+        var secondSelection = $('#second-selection');
+        secondSelection.empty();
+        secondSelection.append('<option>Select Any Ticker</option>');
+        $.each(vals, function (index, value) {
+            secondSelection.append('<option value="' + value + '">' + value + '</option>');
+        });
+    });
+});
+
+$(document).on('change', '#second-selection', function () {
+    $('#first-text' + GBCount).text($('#first-selection').val());
+    $('#second-text' + GBCount).text($('#second-selection').val());
+});
