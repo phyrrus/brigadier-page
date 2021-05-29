f46c8781ae1a682a9a8852d13e8f7a8ca8651377
phyrrus <andrea.pirrone@protonmail.com>
2021-05-29 16:46:31 +0100

    onsubmit in index

--- js/main.js
+++ js/main.js
@@ -166,34 +166,12 @@ function CheckPercentage() {
 }
 
 function CheckInputs() {
+  CheckPercentage();
   if ($("#inputName").val().trim() === "") {
     return $("#inputName").focus();
   }
   if ($("#inputEmail").val().trim() === "") {
     return $("#inputEmail").focus();
-  }
-}
-
-var form = document.querySelector('form');
-
-// Add a listener to our form to wait for its submission
-if (form.addEventListener) {
-  form.addEventListener("submit", validate, false); //Modern browsers
-} else if (form.attachEvent) {
-  form.attachEvent('onsubmit', validate); //Old IE
-}
-
-function validate(e) {
-  CheckInputs;
-  CheckPercentage;
-  var select = e.target.querySelector("select");
-  // Get the value of our selected option
-  var selectedOption = select.options[select.selectedIndex].value;
-  // Compare the value of the default option to the selected option
-  if (selectedOption === "Please Select") {
-    // Trigger Error and prevent the form submission
-    alert("Please select an option!")
-    e.preventDefault();
   }
 }
 
