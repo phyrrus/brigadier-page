8fd0a0a0db1bc4af6c8ecfae87261492755621cb
phyrrus <andrea.pirrone@protonmail.com>
2021-05-29 17:43:18 +0100

    adding document ready

--- js/main.js
+++ js/main.js
@@ -10,48 +10,50 @@ var url = "https://brigadier-testing-functions.azurewebsites.net/api/HttpTrigger
 // Registration form
 // -----------------------------------------------------------------
 
-$("#subscribeform").on('submit', function (event) {
-  event.preventDefault();
-  var Percentage = 0,
-  Sum = 0;
-  for (i = 0; i < $(".inputPercentage").length; i++) {
-    Percentage = Number($(".inputPercentage").eq(i).val().trim());
-    Sum += Percentage;
-  }
-  if (Sum > 100) {
-    $("#percentage-error").html(
-      '<span style="color:red">The portfolio must add up to 100%. Please reduce the proportions.</span>'
-    );
-    return false;
-  } else if (Sum < 100) {
-    $("#percentage-error").html(
-      '<span style="color:red">The portfolio must add up to 100%. Please increase the proportions.</span>'
-    );
-    return false;
-  } else {
-    $("#percentage-error").html("");
-    const firstName = $("#inputName").val();
-    const email = $("#inputEmail").val();
-    var portfolio = {};
-    $('#item-table > tbody').find('tr').each(function() {
-      console.log($(this).find('td:nth-child(2) select').val());
-      console.log($(this).find('td input').val());
-      portfolio[$(this).find('td:nth-child(2) select').val()] = $(this).find('td input').val()
-    });
-    var data = {
-        firstName,
-        email,
-        portfolio
-    };
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: JSON.stringify(data),
-    });
-  }
-  alert('Done! Thank you for registering! You should be receiving a confirmation email shortly. Please check your spam inbox.');
-  location.reload()
+$(document).ready(function () {
+  $("#subscribeform").on('submit', function (event) {
+    event.preventDefault();
+    var Percentage = 0,
+    Sum = 0;
+    for (i = 0; i < $(".inputPercentage").length; i++) {
+      Percentage = Number($(".inputPercentage").eq(i).val().trim());
+      Sum += Percentage;
+    }
+    if (Sum > 100) {
+      $("#percentage-error").html(
+        '<span style="color:red">The portfolio must add up to 100%. Please reduce the proportions.</span>'
+      );
+      return false;
+    } else if (Sum < 100) {
+      $("#percentage-error").html(
+        '<span style="color:red">The portfolio must add up to 100%. Please increase the proportions.</span>'
+      );
+      return false;
+    } else {
+      $("#percentage-error").html("");
+      const firstName = $("#inputName").val();
+      const email = $("#inputEmail").val();
+      var portfolio = {};
+      $('#item-table > tbody').find('tr').each(function() {
+        console.log($(this).find('td:nth-child(2) select').val());
+        console.log($(this).find('td input').val());
+        portfolio[$(this).find('td:nth-child(2) select').val()] = $(this).find('td input').val()
+      });
+      var data = {
+          firstName,
+          email,
+          portfolio
+      };
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: JSON.stringify(data),
+      });
+    }
+    alert('Done! Thank you for registering! You should be receiving a confirmation email shortly. Please check your spam inbox.');
+    location.reload()
   })
+});
 
 // -----------------------------------------------------------------
 // Table
