ef12562c68552b3cd483c931403ff3866f00451b
phyrrus <andrea.pirrone@protonmail.com>
2023-05-07 10:47:40 +0200

    Fixing form and link

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@
 
 $(document).ready(function () {
   $("#subscribeform").on('submit', function () {
-    const email = $("#subEmail").val();
+    const email = $("#email").val();
     var data = {
         email
     };
     $.ajax({
       type: "POST",
-      url: "https://brigadier-form.azurewebsites.net/api/subscribe?",
+      url: "https://brigadier-data.azurewebsites.net/api/form?",
       data: JSON.stringify(data),
     });
     alert('Done! Thank you for registering! You should be receiving a confirmation email shortly. Please check your spam inbox.');
